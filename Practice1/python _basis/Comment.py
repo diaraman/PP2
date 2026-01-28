fbcec341767573c55c81a68e9214a6ae0dc93da5
diaraman <d_aman@kbtu.kz>
2026-01-29 01:05:17 +0500

    Create Comment.py

---
+++ Practice1/python _basis/Comment.py
@@ -0,0 +1,19 @@
+#Here we can see how we can commenting in python
+
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+print("Hello, World!") #This is a comment
+
+#This is a comment
+print("Hello, World!")
+
+
+#also you can add a polyline (triple quotes) to your code and put a comment inside it
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
