1eedaf4f66e8f29d104ea95c3e59f3d6619a2881
diaraman <d_aman@kbtu.kz>
2026-02-05 02:30:34 +0500

    Create if_elif_else.py

---
+++ Practice2/boolean/if_else/if_elif_else.py
@@ -0,0 +1,28 @@
+age = 25
+
+if age < 13:
+    print("You are a child")
+elif age < 20:
+    print("You are a teenager")
+elif age < 65: # returns "You are an adult" and stops here
+    print("You are an adult")
+elif age >= 65:
+    print("You are a senior")
+
+
+day = 3
+
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+elif day == 4:
+    print("Thursday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+    print("Saturday")
+elif day == 7:
+    print("Sunday")
