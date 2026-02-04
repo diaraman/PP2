25e085a5049469c9934746d60c620c05f5ae2021
diaraman <d_aman@kbtu.kz>
2026-02-05 02:44:53 +0500

    Create for_break.py

---
+++ Practice2/loops/for_break.py
@@ -0,0 +1,11 @@
+archons_list = ["venti", "zhongli", "raiden", "nahida", "furina", "mavuika", "mualani"]
+for archon in archons_list:
+    print(archon)
+    if (archon == "Furina"):
+        break
+
+archons_list = ["venti", "zhongli", "raiden", "nahida", "furina", "mavuika", "mualani"]
+for archon in archons_list:
+    if archon == "Mavuika": # 1 step after
+        break
+    print(archon)
