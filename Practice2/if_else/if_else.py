a7f54da27228757780ee4fc62adb2e7f9507aab0
diaraman <d_aman@kbtu.kz>
2026-02-05 02:41:14 +0500

    Add files via upload

---
+++ Practice2/if_else/if_else.py
@@ -0,0 +1,23 @@
+a = 1
+b = 2
+if a > b:
+    print("True")
+elif a == b:
+    print("Equal")
+else:
+    print("False")
+
+
+a = 7
+b = 9
+if a > b:
+    print("Greater")
+else:
+    print("Less")
+
+# if a > b:
+#     print("True")
+# else:
+#     print("False")
+# elif a == b:
+#     print("Equal")
