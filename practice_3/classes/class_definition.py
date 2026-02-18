2d75f7ccca8b6129bc0c2d74209cd8968bc52fb1
diaraman <d_aman@kbtu.kz>
2026-02-19 01:57:43 +0500

    Create class_definition.py

---
+++ practice_3/classes/class_definition.py
@@ -0,0 +1,24 @@
+class Person:
+    def greet(self):
+        print("Hello!")
+
+p = Person()
+p.greet()
+
+
+
+class Car:
+    def start(self):
+        print("Car started")
+
+c = Car()
+c.start()
+
+
+
+class Dog:
+    def bark(self):
+        print("Woof!")
+
+d = Dog()
+d.bark()
