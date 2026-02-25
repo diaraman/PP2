090e56b1dc3d6de75683d662c6dc95683b0bf37b
diaraman <d_aman@kbtu.kz>
2026-02-26 02:13:12 +0500

    Create generators.py

---
+++ Practice4/exercices/generators.py
@@ -0,0 +1,19 @@
+# Iterator and generator exercises
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+#2
+#Create a generator that generates the squares of numbers up to some number N
+def sq(n):
+    for i in range (1,n+1):
+        yield i*i
+n=int(input())
+
+for i in sq(n):
+    print(i,end=" ")
