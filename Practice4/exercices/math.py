94c6c9eecac81ad6f97800a1e58035ff60411bf2
diaraman <d_aman@kbtu.kz>
2026-02-26 02:34:34 +0500

    Create math.py

---
+++ Practice4/exercices/math.py
@@ -0,0 +1,28 @@
+# Math and random operations
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+#2
+x = abs(-7.25)
+
+print(x)
+#3
+import math
+
+x = math.sqrt(64)
+
+print(x)
+#4
+#Import math library
+import math
+
+#Round a number upward to its nearest integer
+x = math.ceil(1.4)
+
+#Round a number downward to its nearest integer
+y = math.floor(1.4)
+
+print(x)
+print(y)
